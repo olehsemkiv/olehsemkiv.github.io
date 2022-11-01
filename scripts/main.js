ba21ef2bff659c0463f80c1e63f1f8318ffa6963
olehsemkiv <olegsemkiv3065@gmail.com>
2022-11-01 19:01:54 +0200

    First Release

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.brands__box-mobile').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500
+    });
+    let check = true;
+    $('.burger-btn').on('click', () => {
+        // console.log('fdsfds');
+        if(check){
+            $('.header__menu-mobile').animate({
+                top: 0
+            })
+            check = false;
+        } else{
+            $('.header__menu-mobile').animate({
+                top: '-100%'
+            })
+            check = true;
+        }
+    })
+})
